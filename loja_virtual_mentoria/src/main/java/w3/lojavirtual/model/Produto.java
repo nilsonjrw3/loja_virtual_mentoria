618ebf2c297db69f82fcfabaa6d62f90e7d18e72
Jose Nilson Ferreira Junior <nilsonjuniorr2@gmail.com>
2024-04-03 21:12:12 -0300

    03-04-2024-02

--- loja_virtual_mentoria/src/main/java/w3/lojavirtual/model/Produto.java
+++ loja_virtual_mentoria/src/main/java/w3/lojavirtual/model/Produto.java
@@ -96,6 +96,18 @@ public class Produto implements Serializable{
 	@JoinColumn(name = "marca_produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "marca_produto_id_fk"))
 	private MarcaProduto marcaProduto = new MarcaProduto();
 	
+	@NotNull(message = "A Nota Item do Produto deve ser informada")
+	@ManyToOne(targetEntity = NotaItemProduto.class)
+	@JoinColumn(name = "nota_item_produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_item_produto_id_fk"))
+	private NotaItemProduto notaItemProduto = new NotaItemProduto();
+	
+	public void setNotaItemProduto(NotaItemProduto notaItemProduto) {
+		this.notaItemProduto = notaItemProduto;
+	}
+	
+	public NotaItemProduto getNotaItemProduto() {
+		return notaItemProduto;
+	}
 	
 	public MarcaProduto getMarcaProduto() {
 		return marcaProduto;
